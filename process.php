684062e0fa46dadda21572c68f201998a43a96a2
Thom Bilton <thombilton@gmail.com>
2018-04-28 14:39:22 +1200

    HTML form completed and the data can be sent to process.php file

---
+++ process.php
@@ -0,0 +1,16 @@
+<?php
+
+include "debug.php";
+
+echo "working";
+
+$fname = $_POST['fname'];
+
+
+echo $fname;
+/**
+ * Created by PhpStorm.
+ * User: Thom
+ * Date: 28/04/18
+ * Time: 1:16 PM
+ */
